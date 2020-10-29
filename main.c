a2ce6572c45380ef07843e11b420762a353b485f
UTFTymoteusz <tymoteusz.paliwoda@gmail.com>
2020-10-29 11:44:29 +0100

    teest

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
+#include <stdbool.h>
+#include <stdio.h>
 #include <unistd.h>
 
 int main(int argc, char* argv[]) {
-    syscall(6);
+    for (int i = 0; i < 5; i++)
+        syscall(251 + i, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE);
+
+    // sleep(3);
+    fwrite("test\n", 5, 5, stdout);
+
     return 0;
 }
