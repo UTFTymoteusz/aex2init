89a7c419544fcdabacbb9dd74ca6c7ea9345eba4
UTFTymoteusz <tymoteusz.paliwoda@gmail.com>
2020-11-20 23:16:15 +0100

    tls is a thing now

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
+#include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
 
+void _set_errno(int);
+
 int main(int argc, char* argv[]) {
+    syscall(126);
+    printf("errno: %i\n", errno);
+
     fwrite("test\n", 5, 1, stdout);
 
     FILE* file = fopen("/sys/test.txt", "r");
